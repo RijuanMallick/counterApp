69f372f0714f3da054d47a5576e6e5666daf1563
Rijuan Mallick <rijuanmallick121@gmail.com>
2026-09-07 00:31:21 +0530

    first commit

---
+++ counterApp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Counter App</div>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => setCount(count - 1)}>dec</button>
+        <button onClick={() => setCount(0)}>reset</button>
+        <button onClick={() => setCount(count + 1)}>inc</button>
+      </div>
+    </>
+  );
+};
+export default App;
