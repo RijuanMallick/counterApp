0e354bae7874578bbf3dd7b42553da9de94a5de2
Rijuan Mallick <rijuanmallick121@gmail.com>
2026-09-07 00:34:51 +0530

    i added sppace here

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <>
       <div>Counter App</div>
       <div>{count}</div>
+
       <div>
         <button onClick={() => setCount(count - 1)}>dec</button>
         <button onClick={() => setCount(0)}>reset</button>
